362a05c1bcf66225bae1200e3126623f69119b91
Dave Fancher <dave@davefancher.com>
2014-10-14 13:01:46 -0400

    Added inheritance example

---
+++ Demos/Inheritance/Example.js
@@ -0,0 +1,39 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var Inheritance;
+(function (Inheritance) {
+    var Person = (function () {
+        function Person(name) {
+            this.name = name;
+        }
+        Person.prototype.identify = function () {
+            return "This person's name is " + this.name;
+        };
+        return Person;
+    })();
+
+    var Developer = (function (_super) {
+        __extends(Developer, _super);
+        function Developer(name) {
+            _super.call(this, name);
+            this.name = name;
+        }
+        Developer.prototype.identify = function () {
+            return "This employee's name is " + this.name;
+        };
+        return Developer;
+    })(Person);
+
+    function RunDemo() {
+        var p1 = new Person("Dave");
+        var p2 = new Developer("Dave");
+
+        return p1.identify() + "<br />" + p2.identify();
+    }
+    Inheritance.RunDemo = RunDemo;
+})(Inheritance || (Inheritance = {}));
+//# sourceMappingURL=Example.js.map
---
+++ Demos/Inheritance/Example.ts
@@ -0,0 +1,27 @@
+﻿module Inheritance {
+  class Person {
+    constructor(public name: string) {
+    }
+
+    identify() {
+      return "This person's name is " + this.name;
+    }
+  }
+
+  class Developer extends Person {
+    constructor(public name: string) {
+      super(name);
+    }
+
+    identify() {
+      return "This employee's name is " + this.name;
+    }
+  }
+
+  export function RunDemo() {
+    var p1 = new Person("Dave");
+    var p2 = new Developer("Dave");
+
+    return p1.identify() + "<br />" + p2.identify();
+  }
+} 
